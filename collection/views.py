a73cc189b9c0898ea75931de88fc9103a521e3a6
suffianhamzah <suffianhamzah@gmail.com>
2017-02-05 22:49:40 -0500

    Added ability to view Things

--- collection/views.py
+++ collection/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request):
-    #this is my new view
-    number = 6
 
-    thing = "Thing name"
-
+    things = Thing.objects.all()
     return render(request, 'index.html', {
-        'number': number,
+        'things': things,
+    })
+
+def thing_detail(request, slug):
+    thing = Thing.objects.get(slug=slug)
+
+    return render(request, 'things/thing_detail.html', {
         'thing': thing,
     })
