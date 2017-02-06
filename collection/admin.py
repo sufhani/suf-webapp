c332484d77b3fe9555ccb8cc3036d8954cb993c6
suffianhamzah <suffianhamzah@gmail.com>
2017-02-05 20:25:14 -0500

    added some models, added more views

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from collection.models import Thing
+
+#set up automated
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepoluted_fields = {'slug': ('name',)}
+
+
+admin.site.register(Thing, ThingAdmin)
--- collection/views.py
+++ collection/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     #this is my new view
-    return render(request, 'index.html')
+    number = 6
+
+    thing = "Thing name"
+
+    return render(request, 'index.html', {
+        'number': number,
+        'thing': thing,
+    })
