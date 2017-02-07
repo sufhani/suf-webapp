85fd18898e623956293982e951335afa1d5a1954
suffianhamzah <suffianhamzah@gmail.com>
2017-02-07 02:01:07 -0500

    Added production settings file

---
+++ sufwebapp1/settings_production.py
@@ -0,0 +1,17 @@
+from sufwebapp1.settings import *
+
+
+import dj_database_url
+DATABASES['default'] dj_database_url.config()
+
+#Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+#Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Set debug to False
+DEBUG = False
+
+# static asset configuration
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- sufwebapp1/wsgi.py
+++ sufwebapp1/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+    "sufwebapp1.settings_production")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sufwebapp1.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
