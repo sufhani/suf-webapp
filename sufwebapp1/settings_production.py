3584fa4a7513cba7f1ad193f00470d57e2e81355
suffianhamzah <suffianhamzah@gmail.com>
2017-02-07 02:02:35 -0500

    Fixed problem

--- sufwebapp1/settings_production.py
+++ sufwebapp1/settings_production.py
@@ -2,7 +2,7 @@ from sufwebapp1.settings import *
 
 
 import dj_database_url
-DATABASES['default'] dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 #Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
